fab24e3c5859ad8ff3f6eb031e072b6abe0f7449
Duong Nguyen <zkoclompe02@gmail.com>
2026-04-17 00:55:23 +0700

    pull về để tạo ra các data items

---
+++ Assets/Script/Items/MaterialItem.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Script.Items
+{
+    [CreateAssetMenu(fileName = "New Material", menuName = "Inventory/Item/Material")]
+    public class MaterialItem : Item
+    {
+        [Header("Crafting Settings")]
+        public bool canBeSmelted; 
+        public Item resultItem;  
+        public float smeltTime = 5f; 
+    }
+}
